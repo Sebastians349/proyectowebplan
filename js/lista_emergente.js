2336177fd150060dbc013faa9e879a6d05e4c6e6
Sebastian Simarro <ssebastian210@gmail.com>
2020-11-24 19:46:36 -0300

    agregada funcion de menu emergente  en script externo

---
+++ js/lista_emergente.js
@@ -0,0 +1,10 @@
+function mostrarLista() {
+	var elemento = document.getElementById('menu_categorias')
+	if (elemento.style.display=='none') {
+		elemento.style.display='block'
+	} else {
+		elemento.style.display='none'
+	}
+}
+
+
